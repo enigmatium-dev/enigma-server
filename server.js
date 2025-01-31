7619f0863331a4659118b6fd78ce47c20cf895e5
enigmatium-dev <enigmatium@proton.me>
2025-01-31 10:52:00 +0200

    update server.js

--- server.js
+++ server.js
@@ -5,13 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
+const socketio_1 = require("./lib/socketio");
+const logger_1 = __importDefault(require("./helpers/logger"));
 const app = (0, express_1.default)();
 const server = app.listen(process.env.PORT);
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+(0, socketio_1.initSocketServer)(server);
+const io = (0, socketio_1.getSocketServer)();
 app.use(`/ping`, (req, res) => {
     res.status(200).json({ message: 'Ping OK' });
 });
 app.use((req, res) => {
     res.status(404).send('Not found');
+});
+io.on('connection', (socket) => {
+    socket.on('message', (data) => {
+        logger_1.default.b('message', data);
+    });
 });
